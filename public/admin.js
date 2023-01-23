e4aee9ac25c84484e9bf5d6c63e14769fda6ef26
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-01-22 19:42:46 -0500

    completed

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,45 @@
-
 // Your Code Here
+// Get all the books
+// Book title, input, button
+// Add event listener to save button to make request to update book
+// Fetch request 
+
+async function main() {
+    const response = await fetch('http://localhost:3001/listBooks') 
+    const books = await response.json()
+
+    books.forEach(renderBook)
+}
+
+function renderBook(book) {
+    console.log(book)
+    const root = document.getElementById('root')
+
+    const li = document.createElement('li')
+    li.textContent = book.title
+
+    const input = document.createElement('input')
+    input.value = book.quantity
+
+    const saveButton = document.createElement('button')
+    saveButton.textContent = 'Save'
+
+    saveButton.addEventListener('click', () => {
+        const body = {
+            id: book.id,
+            quantity: input.value
+        }
+
+        fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        })
+    })
+    li.append(input, saveButton)
+
+    root.appendChild(li)
+}
+main()
--- public/index.js
+++ public/index.js
@@ -26,3 +26,17 @@ function renderBook(book) {
 }
 
 main()
+
+// let response = await fetch('http://localhost:9001/updateBook', {
+//     method: "PATCH",
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//         "id": "3",
+//         "title": "Legends of Arathrae",
+//     }),
+// });
+
+// let updateBook = await response.json()
+// console.log(updateBook)
